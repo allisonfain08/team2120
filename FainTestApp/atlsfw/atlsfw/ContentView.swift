f0b3b290b88b35c44b175096e625188f0a04851c
Fain, Allison Elisabeth <afain8@gatech.edu>
2022-10-05 10:21:04 -0400

    So shonjoy can get his files

---
+++ FainTestApp/atlsfw/atlsfw/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  atlsfw
+//
+//  Created by Allison Fain on 10/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FainTestApp/atlsfw/atlsfw/atlsfwApp.swift
@@ -0,0 +1,17 @@
+//
+//  atlsfwApp.swift
+//  atlsfw
+//
+//  Created by Allison Fain on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct atlsfwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
