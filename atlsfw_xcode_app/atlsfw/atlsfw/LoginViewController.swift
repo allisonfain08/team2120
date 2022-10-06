8a177bf153aceac60bb6e87ef44d6aa6066b192d
Fain, Allison Elisabeth <afain8@gatech.edu>
2022-10-06 10:30:25 -0400

    clean up failed projects

--- atlsfw_xcode_app/atlsfw/atlsfw/LoginViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginViewController.swift
-//  atlsfw
-//
-//  Created by Allison Fain on 10/6/22.
-//
-
-import Foundation
--- atlsfw_xcode_app/atlsfw/atlsfw/SignupViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignupViewController.swift
-//  atlsfw
-//
-//  Created by Allison Fain on 10/6/22.
-//
-
-import Foundation
