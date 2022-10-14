2494effe6e8edaf488a46fef2d6c6dd4f384246e
allisonfain08 <31969795+allisonfain08@users.noreply.github.com>
2022-10-14 01:13:49 -0400

    USE THIS VERSION OF THE APP - WORKS WITH SIMULATOR
    adds app delegate and scene delegate to handle initial setup and control viewcontrollers. follows more traditional xcode (they released a huge update a year ago that moved to swiftui) so this has a ton of stuff that works with storyboards and it widely supported.

---
+++ altsfw-USETHISONE/altsfw/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  altsfw
+//
+//  Created by Allison Fain on 10/13/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let HomeViewController = storyboard.instantiateViewController(identifier: "HomeViewController")
+            
+            // This is to get the SceneDelegate object from your view controller
+            // then call the change root view controller function to change to main tab bar
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(HomeViewController)
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let RegisterViewController = storyboard.instantiateViewController(identifier: "RegisterViewController")
+            
+            // This is to get the SceneDelegate object from your view controller
+            // then call the change root view controller function to change to main tab bar
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(RegisterViewController)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
