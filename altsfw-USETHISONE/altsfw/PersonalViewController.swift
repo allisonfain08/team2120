3b69ae1604d7dd1ad90ae796f7773acb2d99242b
Mija Kennedy <mijakennedy42@gmail.com>
2022-11-06 20:56:38 -0500

    shifting code for drop down
    Correct SwiftUI file found and code for drop down associated with it rather than the created file. Previously created file was removed

--- altsfw-USETHISONE/altsfw/PersonalViewController.swift
+++ altsfw-USETHISONE/altsfw/PersonalViewController.swift
@@ -11,6 +11,18 @@ import SwiftUI
 
 class PersonalViewController: UIViewController {
     
+    @IBOutlet weak var aboutYouLabel: UILabel!
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var ageRestrictLabel: UILabel!
+    
+    @IBOutlet weak var spendingQLabel: UILabel!
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var topCostDropDown: UIButton!
     override func viewDidLoad() {
         struct PersonalViewController: View {
             var body: some View {
@@ -25,6 +37,7 @@ class PersonalViewController: UIViewController {
             func adjustOrder() { }
             func cancelOrder() { }
         }
+        setPopUpButton()
         
      
         // Do any additional setup after loading the view.
@@ -40,5 +53,17 @@ class PersonalViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func setPopUpButton() {
+        let optionClosure = {(action: UIAction) in
+            print(action.title)} /* Can get rid of this once button works, just to test the selection is working -> should print which option was chosen */
+        
+        topCostDropDown.menu = UIMenu(children : [
+            UIAction(title: "Under $50", state : .on,  handler: optionClosure),
+            UIAction(title: "$50 - $100",  handler: optionClosure),
+            UIAction(title: "$100 - $150",  handler: optionClosure),
+            UIAction(title: "$150 - $200",  handler: optionClosure),
+            UIAction(title: "More than $200",  handler: optionClosure)])
+        topCostDropDown.showsMenuAsPrimaryAction = true;
+        topCostDropDown.changesSelectionAsPrimaryAction = true;
+    }
 }
