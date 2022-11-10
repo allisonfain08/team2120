9fff901575ac9aa04a6c6fff1757716cbe6afd75
Mija Kennedy <mijakennedy42@gmail.com>
2022-11-09 22:04:36 -0500

    quiz intro back button fix
    Quiz intro page back button now working.

--- altsfw-USETHISONE/altsfw/BeginQuizViewController.swift
+++ altsfw-USETHISONE/altsfw/BeginQuizViewController.swift
@@ -15,4 +15,27 @@ class BeginQuizViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var outerRectangleFill: UIImageView!
+    @IBOutlet weak var rectangleFill: UITextView!
+    @IBOutlet weak var styleLabel: UILabel!
+    @IBOutlet weak var whatLabel: UILabel!
+    
+    @IBOutlet weak var quizDescriptionLabel: UITextView!
+    
+    @IBOutlet weak var eklozetLogo: UIImageView!
+    
+    
+    @IBAction func back(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let HomeViewController = storyboard.instantiateViewController(identifier: "HomeViewController")
+            
+            // This is to get the SceneDelegate object from your view controller
+            // then call the change root view controller function to change to main tab bar
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(HomeViewController)
+    }
+    
+    @IBAction func beginButton(_ sender: Any) {
+    }
+    
+    
 }
