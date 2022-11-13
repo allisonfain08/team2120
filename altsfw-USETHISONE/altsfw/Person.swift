1243ba56ef64acc884f8b5f9ea3cdd8831c4ded2
Mija Kennedy <mijakennedy42@gmail.com>
2022-11-13 15:34:46 -0500

    Database Entry Setup Personal Info
    Added commented example code to temporarily demonstrate where database should be modified to add the budget information from the quiz.

--- altsfw-USETHISONE/altsfw/Person.swift
+++ altsfw-USETHISONE/altsfw/Person.swift
@@ -13,9 +13,42 @@ class Person {
     // username, password
     var username: String = ""
     var password: String = ""
+    var topBudget: String =  ""
+    var bottomBudget: String = ""
+    var outerBudget: String = ""
+    var dressBudget: String = ""
+    var shoeBudget: String = ""
+    var accBudget: String = ""
+    var jewelryBudget: String = ""
     
     init(username:String, password:String) {
         self.username = username
         self.password = password
+    }
+    func setTopBudget(topBudget:String) {
+        self.topBudget = topBudget
+    }
+    
+    func setBottomBudget(bottomBudget:String) {
+        self.bottomBudget = bottomBudget
+    }
+    
+    func setOuterBudget(outerBudget:String) {
+        self.outerBudget = outerBudget
+    }
+    
+    func setDressBudget(dressBudget:String) {
+        self.dressBudget = dressBudget
+    }
+    
+    func setShoeBudget(shoeBudget:String) {
+        self.shoeBudget = shoeBudget
+    }
+    func setAccBudget(accBudget:String) {
+        self.accBudget = accBudget
+    }
+    
+    func setJewelryBudget(jewelryBudget:String) {
+        self.jewelryBudget = jewelryBudget
     }
 }
