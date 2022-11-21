6aeb589ebeba13f206e0aef1645850ff1cc30e97
Fain, Allison Elisabeth <afain8@gatech.edu>
2022-11-21 10:34:41 -0500

    11/21

---
+++ altsfw-USETHISONE/altsfw/ArticleOfClothing 2.swift
@@ -0,0 +1,25 @@
+//
+//  ArticleOfClothing.swift
+//  altsfw
+//
+//  Created by Shonjoy Bhattacharyya on 11/12/22.
+// This represents each article of clothing : image, size, type of clothing, color 1 and 2, material 1 and 2
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ArticleOfClothing {
+    var type,primaryMaterial,secondaryMaterial, primaryColor,secondaryColor,size : String
+    var image : UIImage
+    
+    init(type: String, primaryMaterial: String, secondaryMaterial: String, primaryColor: String, secondaryColor: String, size: String, image: UIImage) {
+        self.type = type
+        self.primaryMaterial = primaryMaterial
+        self.secondaryMaterial = secondaryMaterial
+        self.primaryColor = primaryColor
+        self.secondaryColor = secondaryColor
+        self.size = size
+        self.image = image
+    }
+}
---
+++ altsfw-USETHISONE/altsfw/ClosetArray 2.swift
@@ -0,0 +1,34 @@
+//
+//  ClosetArray.swift
+//  altsfw
+//
+//  Created by Shonjoy Bhattacharyya on 11/12/22.
+//
+//This is the array of all clothes in a user's closet, NOT the outfits
+
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class ClosetArray {
+    var closet: [ArticleOfClothing] = []
+    func addItem(item : ArticleOfClothing) {
+        closet.append(item)
+    }
+    func printCloset() {
+        print(closet.description)
+    }
+    func returnClosetString() -> String {
+        return closet.description
+    }
+    func returnCloset() -> [ArticleOfClothing] {
+        return closet
+    }
+    
+    func getArticleOfClothing(index: Int) -> ArticleOfClothing {
+        return closet[index]
+    }
+}
+
+
